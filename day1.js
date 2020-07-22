ab0dd0141945cdddc42f80eebf2c5fb59c205c3f
sugeshboosan <68215042+sugeshboosan@users.noreply.github.com>
2020-07-22 20:17:47 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,43 @@
+
+// types of console
+for (i = 0; i < 10; i++) {
+    console.count();
+  }
+
+
+  console.log("Hello world!");
+  console.group();
+  console.log("Hello again, this time inside a group!")
+
+
+  console.info("Hello letsupgrade!");
+
+
+  // var obj and const
+  var a=1
+ let b=2
+ if (a===1){
+     let a=10
+     let b=20
+     console.log(a)
+     console.log(b)
+ }
+ console.log(a)
+ console.log(b)
+
+ 
+ const obj = {name:"sugi"};
+  console.log(obj.name)
+
+  let getArroevalue = function(){
+      return 10;
+  } 
+  console.log(getArroevalue())
+
+
+
+  /*
+datatypes
+
+primitive:number,string,boolean,undefined,null
+reference:object,function,array*/
